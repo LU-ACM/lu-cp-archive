957d5659ec2416715bfd15178f7bd7b444b493ba
Iftekhar-Ifat <iftekharifat007@gmail.com>
2026-03-17 19:55:52 +0600

    updated packages and added types for dom-to-image-more

---
+++ dom-to-image-more.d.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+declare module "dom-to-image-more" {
+  export interface Options {
+    filter?: (node: Node) => boolean;
+    bgcolor?: string;
+    width?: number;
+    height?: number;
+    style?: object;
+    quality?: number;
+    imagePlaceholder?: string;
+    cacheBust?: boolean;
+    [key: string]: any;
+  }
+
+  export function toPng(node: Node, options?: Options): Promise<string>;
+  export function toJpeg(node: Node, options?: Options): Promise<string>;
+  export function toBlob(node: Node, options?: Options): Promise<Blob>;
+  export function toPixelData(
+    node: Node,
+    options?: Options
+  ): Promise<Uint8ClampedArray>;
+  export function toSvg(node: Node, options?: Options): Promise<string>;
+
+  const domToImage: {
+    toPng: typeof toPng;
+    toJpeg: typeof toJpeg;
+    toBlob: typeof toBlob;
+    toPixelData: typeof toPixelData;
+    toSvg: typeof toSvg;
+  };
+
+  export default domToImage;
+}
